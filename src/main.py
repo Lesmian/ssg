95339fdafff2f92cb0499782ff9cdec6cfc1cb1f
Lesmian <lesmian3@gmail.com>
2025-01-12 13:58:59 +0100

    copy static to public dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import os
+import shutil
 from leafnode import LeafNode
 from textnode import *
 from utils import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 def main():
-    pass
+    copy_source_to_destination("static", "public")
 
 def text_node_to_html_node(text_node):
     match text_node.text_type:
@@ -29,5 +31,19 @@ def text_to_textnodes(text):
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
+
+def copy_source_to_destination(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    
+    os.mkdir(destination)
+    paths = os.listdir(source)
+    for path in paths:
+        sourcePath = os.path.join(source, path)
+        destinationPath = os.path.join(destination, path)
+        if os.path.isfile(sourcePath):
+            shutil.copy(sourcePath, destinationPath)
+        else:
+            copy_source_to_destination(sourcePath, destinationPath)
 
 main()
